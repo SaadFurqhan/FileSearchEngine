82fd1d76276d05b42d16b397e9f0148397729a5b
SAAD <57146004+SaadFurqhan@users.noreply.github.com>
2020-12-28 21:59:05 +0530

    Update view_file.py

--- view_file.py
+++ view_file.py
@@ -181,6 +181,4 @@ def no_of_drives():
 
 # main function
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-#     app.run(debug=True)
+    app.run(debug=True)
